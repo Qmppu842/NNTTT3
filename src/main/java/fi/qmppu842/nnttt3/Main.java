51ccd18555b1ad5ce3b766ecf08cc8ed5063c6ce
Qmppu842 <omppu842@gmail.com>
2018-06-29 15:33:56 +0300

    Setting (hopefully) last depencies. First tests implemented

--- src/main/java/fi/qmppu842/nnttt3/Main.java
+++ src/main/java/fi/qmppu842/nnttt3/Main.java
@@ -1,5 +1,8 @@
 package fi.qmppu842.nnttt3;
 
+import fi.qmppu842.nnttt3.Game.GameStateBean;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -20,28 +23,42 @@ public class Main extends Application {
         Button btn = new Button();
         btn.setText("Say 'Hello World'");
         btn.setOnAction(new EventHandler<ActionEvent>() {
-
+            
             @Override
             public void handle(ActionEvent event) {
                 System.out.println("Hello World!");
             }
         });
-
+        
         StackPane root = new StackPane();
         root.getChildren().add(btn);
-
+        
         Scene scene = new Scene(root, 300, 250);
-
+        
         primaryStage.setTitle("Hello World!");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
 
+        static int testee = 0;
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         launch(args);
-    }
 
+        GameStateBean board = new GameStateBean();
+        PropertyChangeListener liste = new PropertyChangeListener() {
+            @Override
+            public void propertyChange(PropertyChangeEvent pce) {
+                System.out.println("moi!");
+                testee++;
+            }
+        };
+//        board.addPropertyChangeListener(liste);
+//        board.setBoard(3, 3);
+//        board.setBoard(3, 4);
+        
+    }
+    
 }
